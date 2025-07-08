f98dbbc305d42ce12def53ece7a257245cfa01f6
PriyankaKadam09 <piu972004@gmail.com>
2025-07-08 16:59:08 +0530

    Delete Assignment directory

--- Assignment/Assignment3/Question1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main ()
-{
-	int i;
-	for(int i=1; i<=10;i++){
-	printf("%d\n",i);
-	
-	}
-}
--- Assignment/Assignment3/Question3.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int start=1,end=5;
-	for(int i =1; i<=15; i++){
-		printf("%d \n",i);
-	}
-}
